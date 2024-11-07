7533b87b83b3d917ef3e243bde2baed86bb2c027
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2024-11-07 15:12:58 -0700

    End of Class Nov 6

--- HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerEdit.razor.cs
+++ HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerEdit.razor.cs
@@ -1,6 +1,7 @@
 ﻿using HogWildSystem.BLL;
 using HogWildSystem.ViewModels;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using static MudBlazor.Icons;
 
 namespace HogWildWebApp.Components.Pages.SamplePages
@@ -12,7 +13,16 @@ namespace HogWildWebApp.Components.Pages.SamplePages
 
         // The Customer
         private CustomerEditView customer = new();
-
+        //  The provinces
+        private List<LookupView> provinces = new();
+        //  The countries
+        private List<LookupView> countries = new();
+        //  The status lookup
+        private List<LookupView> statusLookup = new();
+        // the edit context
+        private EditContext editContext;
+        // close button text
+        private string closeButtonText;
         #endregion
 
         #region Feedback & Error Messages
@@ -39,6 +49,9 @@ namespace HogWildWebApp.Components.Pages.SamplePages
         //  The customer service
         [Inject] protected CustomerService CustomerService { get; set; }
 
+        //  The category lookup service
+        [Inject] protected CategoryLookupService CategoryLookupService { get; set; }
+
         //  Customer ID used to create or edit a customer
         [Parameter] public int CustomerID { get; set; } = 0;
 
@@ -49,12 +62,21 @@ namespace HogWildWebApp.Components.Pages.SamplePages
             await base.OnInitializedAsync();
             try
             {
+                //  edit context needs to be setup after data has been initialized
+                //  setup of the edit context to make use of the payment type property
+                editContext = new EditContext(customer);
+
                 // check to see if we are navigating using a valid customer CustomerID
-                //      or arw we going to create a new customer
+                //      or are we going to create a new customer
                 if (CustomerID > 0)
                 {
                     customer = CustomerService.GetCustomer(CustomerID);
                 }
+
+                // lookups
+                provinces = CategoryLookupService.GetLookups("Province");
+                countries = CategoryLookupService.GetLookups("Country");
+                statusLookup = CategoryLookupService.GetLookups("Customer Status");
 
                 await InvokeAsync(StateHasChanged);
             }
@@ -81,6 +103,50 @@ namespace HogWildWebApp.Components.Pages.SamplePages
                     errorDetails.Add(error.Message);
                 }
             }
+        }
+
+        private void Save()
+        {
+            try
+            {
+                //  reset the error detail list
+                errorDetails.Clear();
+
+                //  reset the error message to an empty string
+                errorMessage = string.Empty;
+
+                //  reset feedback message to an empty string
+                feedbackMessage = string.Empty;
+
+               
+            }
+            catch (ArgumentNullException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (ArgumentException ex)
+            {
+                errorMessage = BlazorHelperClass.GetInnerException(ex).Message;
+            }
+            catch (AggregateException ex)
+            {
+                //  have a collection of errors
+                //  each error should be place into a separate line
+                if (!string.IsNullOrWhiteSpace(errorMessage))
+                {
+                    errorMessage = $"{errorMessage}{Environment.NewLine}";
+                }
+                errorMessage = $"{errorMessage}Unable to search for customer";
+                foreach (var error in ex.InnerExceptions)
+                {
+                    errorDetails.Add(error.Message);
+                }
+            }
+        }
+
+        private async void Cancel()
+        {
+
         }
     }
 }
