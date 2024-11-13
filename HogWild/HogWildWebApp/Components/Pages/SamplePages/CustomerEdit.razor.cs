c290937fe0c037d03a7c829215155dc4e0e8d5e5
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2024-11-13 12:28:35 -0700

    End of class including Simple Dialog

--- HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerEdit.razor.cs
+++ HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerEdit.razor.cs
@@ -21,11 +21,36 @@ namespace HogWildWebApp.Components.Pages.SamplePages
         private List<LookupView> statusLookup = new();
         // close button text
         private string closeButtonText = "Close";
-
         // close button color
         private Color closeButtonColor = Color.Success;
+        // disable the save button if customer is not modified or validate (we have errors)
+        private bool disableSaveButton => !editContext.IsModified() || !editContext.Validate();
         // the edit context
         private EditContext editContext;
+        //  used to store the validation message
+        private ValidationMessageStore messageStore;
+
+        /// <summary>
+        /// The show dialog
+        /// </summary>
+        private bool showDialog = false;
+        /// <summary>
+        /// The dialog message
+        /// </summary>
+        private string dialogMessage = string.Empty;
+        /// <summary>
+        /// The dialog completion source
+        /// </summary>
+        private TaskCompletionSource<bool?> dialogCompletionSource;
+
+        /// <summary>
+        /// Shows the dialog.
+        /// </summary>
+        private async Task ShowDialog()
+        {
+            dialogMessage = "Do you wish to close the invoice editor?";
+            showDialog = true;
+        }
         #endregion
 
         #region Feedback & Error Messages
@@ -77,6 +102,15 @@ namespace HogWildWebApp.Components.Pages.SamplePages
                 //  setup of the edit context to make use of the payment type property
                 editContext = new EditContext(customer);
 
+                //  set the validation to use the HandleValidationRequest event
+                editContext.OnValidationRequested += HandleValidationRequested;
+
+                //  setup the message store to track any validation messages
+                messageStore = new ValidationMessageStore(editContext);
+
+                //  this event will fire each time the data in a property has change.
+                editContext.OnFieldChanged += EditContext_OnFieldChanged;
+
                 // check to see if we are navigating using a valid customer CustomerID
                 //      or are we going to create a new customer
                 if (CustomerID > 0)
@@ -114,6 +148,46 @@ namespace HogWildWebApp.Components.Pages.SamplePages
                     errorDetails.Add(error.Message);
                 }
             }
+        }
+
+        // Handles the validation requested.        
+        private void HandleValidationRequested(object sender, ValidationRequestedEventArgs e)
+        {
+            //  clear the message store if there is any existing validation errors.
+            messageStore?.Clear();
+
+            //  custom validation logic
+            //  first name is required
+            if (string.IsNullOrWhiteSpace(customer.FirstName))
+            {
+                messageStore?.Add(() => customer.FirstName, "First Name is required!");
+            }
+            //  last name is required
+            if (string.IsNullOrWhiteSpace(customer.LastName))
+            {
+                messageStore?.Add(() => customer.LastName, "Last Name is required!");
+            }
+            //  phone is required
+            if (string.IsNullOrWhiteSpace(customer.Phone))
+            {
+                messageStore?.Add(() => customer.Phone, "Phone is required!");
+            }
+            //  email is required
+            if (string.IsNullOrWhiteSpace(customer.Email))
+            {
+                messageStore?.Add(() => customer.Email, "Email is required!");
+            }
+        }
+
+        // Handles the OnFieldChanged event of the EditContext control.
+        private void EditContext_OnFieldChanged(object sender, FieldChangedEventArgs e)
+        {
+            //  the "editContext.Validate()" should not be needed
+            //    but if the "HandleValidationRequested" does not fire on it own
+            //    you will need to add it.  
+            editContext.Validate();
+            closeButtonText = editContext.IsModified() ? "Cancel" : "Close";
+            closeButtonColor = editContext.IsModified() ? Color.Warning : Color.Default;
         }
 
         private void Save()
@@ -158,9 +232,43 @@ namespace HogWildWebApp.Components.Pages.SamplePages
             }
         }
 
-        private async void Cancel()
+        /// <summary>
+        /// Closes this instance.
+        /// </summary>
+        private async Task Cancel()
         {
-            NavigationManager.NavigateTo("/SamplePages/CustomerList/");
+            // Initialize the TaskCompletionSource
+            dialogCompletionSource = new TaskCompletionSource<bool?>();
+            dialogMessage = "Do you wish to close the customer editor?";
+            showDialog = true;
+            bool? results = await ShowDialogAsync();
+            if ((bool)results)
+            {
+                NavigationManager.NavigateTo($"/SamplePages/CustomerList");
+            }
+        }
+
+        /// <summary>
+        /// Show dialog as an asynchronous operation.
+        /// </summary>
+        /// <returns>A Task&lt;System.Boolean&gt; representing the asynchronous operation.</returns>
+        private async Task<bool?> ShowDialogAsync()
+        {
+            // Initialize the TaskCompletionSource
+            dialogCompletionSource = new TaskCompletionSource<bool?>();
+
+            // Wait for the dialog to be closed and return the result
+            return await dialogCompletionSource.Task;
+        }
+
+        /// <summary>
+        /// Simples the dialog result.
+        /// </summary>
+        /// <param name="result">if set to <c>true</c> [result].</param>
+        private void SimpleDialogResult(bool? result)
+        {
+            showDialog = false;
+            dialogCompletionSource.SetResult(result);
         }
     }
 }
